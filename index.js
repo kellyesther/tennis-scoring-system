18c5891da1c4f847fe0ffede1f3b581e3c3677e4
kellyesther97 <kellyesther97@gmail.com>
2022-03-10 00:20:12 +1300

    compile index

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ var Match = /** @class */ (function () {
         this.player1 = { name: player1, points: 0, gamePoints: 0 };
         this.player2 = { name: player2, points: 0, gamePoints: 0 };
     }
+    /**
+     * Translate a point to corresponding score value
+     **/
     Match.prototype.convertPoints = function (point) {
-        //translate a point to corresponding score value
         var scores = {
             0: 0,
             1: 15,
@@ -18,75 +20,77 @@ var Match = /** @class */ (function () {
         return scores[point];
     };
     Match.prototype.deuce = function () {
-        //returns true if at least 3 points have been scored by each player and scores are equal
         return (this.player1.points >= 3 && this.player2.points === this.player1.points);
     };
+    /**
+     * Check for advantage
+     * If both players have at least 3 points and a player has one more than the opponent, that player has 'Advantage'
+     */
     Match.prototype.advantage = function () {
-        // If at least 3 points have been scored by each side and a player has one more point than his opponent, the score of the game is "advantage" for the player in the lead.
         if (this.player1.points >= 3 &&
             this.player1.points === this.player2.points + 1) {
-            //player 1 has advantage
             return true;
         }
         if (this.player2.points >= 3 &&
             this.player2.points === this.player1.points + 1) {
-            //player 2 has advantage
             return true;
         }
         return false;
     };
+    /**
+     * Return game winner by comparing who has a higher point
+     */
     Match.prototype.gameWinner = function () {
-        //check winner by comparing who has a higher point
-        // console.log(this.player1.gamePoints);
-        // console.log(this.player2.gamePoints);
-        // console.log(this.tieBreak);
+        //
         if (this.player1.points > this.player2.points) {
             return this.player1.name;
         }
         return this.player2.name;
     };
+    /**
+     * Return set winner by comparing who has a higher game point
+     */
     Match.prototype.setWinner = function () {
         if (this.player1.gamePoints > this.player2.gamePoints) {
             return this.player1.name;
         }
         return this.player2.name;
     };
+    /**
+     * Reset points to zero for starting a new game
+     */
     Match.prototype.resetPoints = function () {
-        //reset points to zero for starting a new game
         this.player1.points = 0;
         this.player2.points = 0;
     };
+    /**
+     * check if game ends based on players points and including event of tiebreak
+     */
     Match.prototype.gameEnded = function () {
-        //A game is won by the first player to have won at least 4 points in total and at least 2 points more than the opponent.
         if (!this.tieBreak) {
-            //   console.log("am i in tie break");
             if (this.player1.points >= 4 &&
                 this.player1.points - this.player2.points >= 2) {
-                // player 1 won
                 return true;
             }
             if (this.player2.points >= 4 &&
                 this.player2.points - this.player1.points >= 2) {
-                // player 2 won
                 return true;
             }
         }
-        //in event of tiebreak game ends when player scores at least 7 points in total and at least 2 points more than the opponent
         if (this.player1.points >= 7 &&
             this.player1.points - this.player2.points >= 2) {
-            // player 1 won tiebreak
             return true;
         }
         if (this.player2.points >= 7 &&
             this.player2.points - this.player1.points >= 2) {
-            // player 2 won tiebreak
             return true;
         }
         return false;
     };
+    /**
+     * check how a set is finished including tiebreak event
+     */
     Match.prototype.setWon = function () {
-        // console.log(this.player1.gamePoints);
-        // console.log(this.player2.gamePoints);
         if (this.player1.gamePoints === 6 &&
             this.player1.gamePoints === this.player2.gamePoints) {
             this.tieBreak = true;
@@ -104,8 +108,10 @@ var Match = /** @class */ (function () {
         }
         return false;
     };
+    /**
+     * Checks which player won the point and increament their point
+     */
     Match.prototype.pointWonBy = function (player) {
-        //checks which player won the point and increament their points
         if (player === this.player1.name) {
             this.player1.points += 1;
         }
@@ -113,45 +119,39 @@ var Match = /** @class */ (function () {
             this.player2.points += 1;
         }
     };
+    /**
+     * Returns current match result.
+     * Results have different format for different situation i.e set won, deuce, advantage, a game won, tiebreak or running game
+     */
     Match.prototype.score = function () {
         if (this.setWon()) {
-            //   console.log("did i win");
             return "Winner of tennis match is " + "".concat(this.setWinner());
         }
-        //check if game has ended and return game point results
         if (this.gameEnded()) {
             if (this.gameWinner() === this.player1.name) {
-                //player 1 wins, increament player 1 game point
                 this.player1.gamePoints += 1;
-                //reset points to start a new game
                 this.resetPoints();
             }
             else if (this.gameWinner() === this.player2.name) {
-                //player 1 wins, increament player 1 game point
                 this.player2.gamePoints += 1;
-                //reset points to start a new game
                 this.resetPoints();
             }
             return "".concat(this.player1.gamePoints, "-") + "".concat(this.player2.gamePoints, " ");
         }
-        //if game still ongoing check deuce and return score
         if (!this.tieBreak && this.deuce()) {
             return ("".concat(this.player1.gamePoints, "-") + "".concat(this.player2.gamePoints, ", ") + "Deuce");
         }
-        //if game still ongoing check advantage and return score
         if (!this.tieBreak && this.advantage()) {
             return ("".concat(this.player1.gamePoints, "-") +
                 "".concat(this.player2.gamePoints, ", ") +
                 "Advantage ".concat(this.gameWinner()));
         }
-        //if game still ongoing is a tiebreak, points are increament by 1, eg: 6-6, 0-1
         if (this.tieBreak) {
             return ("".concat(this.player1.gamePoints, "-") +
                 "".concat(this.player2.gamePoints, ", ") +
                 "".concat(this.player1.points, "-") +
                 "".concat(this.player2.points));
         }
-        //return score with overall game points followed by current game standing points, eg: 0-0, 0-15
         return ("".concat(this.player1.gamePoints, "-") +
             "".concat(this.player2.gamePoints, ", ") +
             "".concat(this.convertPoints(this.player1.points), "-") +
